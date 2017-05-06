fac515418fdbf69df378d90c0560680c0a8b23c6
sdiabate <diabate_seriba@yahoo.fr>
2017-05-07 00:04:29 +0200

    Profiles management

--- workspace/timetable/timetable-business/src/test/java/com/bougouri/timetable/business/AbstractTest.java
+++ workspace/timetable/timetable-business/src/test/java/com/bougouri/timetable/business/AbstractTest.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.bougouri.timetable.business.model.Appointment;
@@ -20,6 +21,7 @@ import com.bougouri.timetable.business.service.impl.BasicDaoService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Starter.class)
+@ActiveProfiles("dev")
 public class AbstractTest {
 
 	@Autowired
